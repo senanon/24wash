b9f712c2be3727b2db4b8be3d7138611224210ff
senanon <38965602+senanon@users.noreply.github.com>
2026-07-30 15:52:49 +0700

    Update promptpay.js

--- promptpay.js
+++ promptpay.js
@@ -3,3 +3,25 @@
 ========================================================== */
 
 console.log("PromptPay Engine Loaded");
+
+// รอให้หน้าเว็บโหลดเสร็จ
+window.addEventListener("DOMContentLoaded", () => {
+
+    const qrBox = document.getElementById("qrContainer");
+
+    if (!qrBox) {
+        console.log("ไม่พบ qrContainer");
+        return;
+    }
+
+    qrBox.innerHTML = "";
+
+    new QRCode(qrBox, {
+        text: "24WASH TEST",
+        width: 260,
+        height: 260
+    });
+
+    console.log("QR Created");
+
+});
